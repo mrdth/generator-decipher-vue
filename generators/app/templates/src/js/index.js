4aa9c37d0c876cd1be0d33002e5ff6567fd371ed
mrdth <mrdth@zeventien.org>
2019-11-07 15:19:46 +0000

    Switch to using EcmaScript imports instead of CommonJS

--- generators/app/templates/src/js/index.js
+++ generators/app/templates/src/js/index.js
@@ -1,7 +1,8 @@
 /* global require */
 import Vue from 'vue';
+import <%= toolName %> from './components/<%= toolName %>.vue'
 
-Vue.component('<%= toolName %>-decipher', require('./components/<%= toolName %>.vue'));
+Vue.component('<%= toolName %>-decipher', <%= toolName %>);
 
 // Boot Vue.js, and attach to the top level #survey element.
 window.addEventListener('load', (event) => {
